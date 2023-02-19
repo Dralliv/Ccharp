b1e8104ee43ed7dea3e6e5a7187f98d47a786567
Jean  Dralliv <45214590+Dralliv@users.noreply.github.com>
2023-02-19 12:51:29 -0300

    exercicio 6

---
+++ exe6/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Bem vindo a Tabuada");
+Console.WriteLine("Digite o Valor");
+int valor  = int.Parse (Console.ReadLine());
+int tabuada = 11;
+int[] array = new int[tabuada];
+for(int i = 0; i < tabuada; i++){
+Console.WriteLine(valor +" x "+i+" = "+ i*valor );
+}
+
+
+
+
+/*int tamanhoArray = 3;
+
+string[] array  = new string[tamanhoArray];
+
+
+Console.WriteLine("escreva " + tamanhoArray+ " nomes:");
+for(int i = 0; i < tamanhoArray; i++){
+  array[i] = Console.ReadLine();
+}
+for(int i = 0; i < tamanhoArray; i++){
+  Console.WriteLine(i + "posição - nome: " + array[i]);
+}*/
