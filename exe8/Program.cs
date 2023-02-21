2801320b9b14d38c005b82c430b762e84dee3941
Jean  Dralliv <45214590+Dralliv@users.noreply.github.com>
2023-02-21 11:36:41 -0300

    acrescentado exe8 e 9

---
+++ exe8/Program.cs
@@ -0,0 +1,13 @@
+﻿int numeroAnterior = 0;
+int numeroAtual = 1;
+int novoNumero;
+int fibonacci;
+Console.WriteLine  ("Digite um numero para quantidade de seguencias");
+novoNumero = int.Parse(Console.ReadLine());   
+   for (int i = 0; i <novoNumero;  i++ )
+ {  
+       fibonacci = numeroAnterior + numeroAtual; 
+       Console.WriteLine  (fibonacci);
+       numeroAnterior = numeroAtual; 
+       numeroAtual = fibonacci;   
+  }
